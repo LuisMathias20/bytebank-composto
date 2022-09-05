5f04921fc357e79f43110b783cc31e365417fa94
LuisMathias20 <luiscraft15@gmail.com>
2022-09-05 19:49:13 -0300

    First commit

---
+++ src/bytebank/TestaClasse.java
@@ -0,0 +1,17 @@
+package bytebank;
+
+public class TestaClasse {
+
+	public static void main(String[] args) {
+		Cliente paulo = new Cliente();
+		
+		paulo.nome = "Paulo Silveira";
+		paulo.cpf = "222.222.222-22";
+		paulo.profissao = "Programador";
+		
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.titular = paulo;
+	}
+	
+
+}
